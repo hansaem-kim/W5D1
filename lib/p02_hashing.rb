32c8ea55cfbb3bb6ab488f6c20a0e9bf302b1b5e
Hansaem Kim <hansaem_kim@hotmail.com>
2021-07-12 12:07:27 -0400

    Hansaem 1

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    num = 0
+    self.each.with_index do |ele, index|
+      num += ele.hash ^ index
+    end
+    num.hash
   end
 end
 
 class String
   def hash
+    num = 0
+    self.each_char.with_index {|char , index| num += char.ord ^ index}
+    num.hash
   end
 end
 
@@ -16,6 +24,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    num = 0
+    self.each {|key, value| num += key.hash ^ value.hash}
+    num
   end
 end
